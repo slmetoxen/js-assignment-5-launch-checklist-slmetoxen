9a071c2d6a8bd5053375270f03104553347e1593
Sarah <slmetoxen@gmail.com>
2023-07-05 14:30:12 -0500

    Slow and Steady

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Write your JavaScript code here!
 
-/*const { addDestinationInfo, formSubmission, validateInput, pickPlanet, myFetch  } = require("./scriptHelper");
+const { addDestinationInfo, formSubmission, validateInput, pickPlanet, myFetch  } = require("./scriptHelper");
 /* ??? validateInput & formSubmission are not being read can I use document.querySelectorAll*/
 window.addEventListener("load", function() {
 
@@ -15,12 +15,23 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
        let planetSelected = pickPlanet(listedPlanets);
        addDestinationInfo(document,planetSelected.name,planetSelected.diameter,planetSelected.star, planetSelected.distance, planetSelected.moons, planetSelected.image);
-   })
- /*  let form = document.querySelector("form");
-      form.addEventListener("submit", function(event) {
-         let pilotNameInput = document.querySelector("input[name=pilotName]");
-         let copilotNameInput = document.querySelector("input[name=copilotName]");
-         let fuelInput = document.querySelector("input[name=fuelLevel]");
-         let cargoInput = document.querySelector("input[name=cargoMass]");
-      });*/
+   });
+    let list = document.getElementById("faultyItems");
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let pilotNameInput = document.querySelector("input[name=pilotName]");
+        let pilot = pilotNameInput.value;
+
+        let copilotNameInput = document.querySelector("input[name=copilotName]");
+        let copilot = copilotNameInput.value;
+
+        let fuelInput = document.querySelector("input[name=fuelLevel]");
+        let fuelLevel = fuelInput.value;
+
+        let cargoInput = document.querySelector("input[name=cargoMass]");
+        let cargoLevel = cargoInput.value;
+
+        /*alert("All fields are required!");*/
+        event.preventDefault();
+      });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,5 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+/*require('isomorphic-fetch');*/
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    /* Here is the HTML formatting for our mission target div.
@@ -18,6 +18,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 /* MY COMMENTS FROM DIRECTIONS---validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" ...use validateInput() to complete the formSubmission() function */
 function validateInput(testInput) {
+    /* because input will return as a string I have to make it a number first */
     let input = Number(testInput);
     if (input === '') {
         return "Empty";
@@ -37,32 +38,45 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let copilotStatus = document.getElementById("copilotStatus");
     let fuelStatus = document.getElementById("fuelStatus");
     let cargoStatus = document.getElementById("cargoStatus");
-    let list = document.getElementById("faultyItems");
+    /*let list = document.getElementById("faultyItems"); ??? Does this go in script instead?*/
     let launchStatus = document.getElementById("launchStatus");
-    /*??? Why aren't fuelStatus, cargoStatus, and launchStatus being read?*/
+   
     
 
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
         alert ("All fields are required.");
     } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
         alert ("Please enter vaild information for all required fields.")
-    } else {
+    } /* ??? directions say if the user submits a fuel level that is too low (less than 10,000 liters), change faultyItems to visible ??ex) element.style.aStyleProperty ?? with an updated fuel status stating that there is not enough fuel for the journey. launchStatus, should also change to "Shuttle not ready for launch" and the color should change to red.*/
+     else {
+        div.style.visibility = "visible";
+
         pilotStatus.innerHTML = `Pilot${pilot} is ready for launch.`;
         copilotStatus.innerHTML = `Co-pilot${copilot} is ready for launch.`;
-        /* ??? directions say if the user submits a fuel level that is too low (less than 10,000 liters), change faultyItems to visible? ex) element.style.aStyleProperty ?? with an updated fuel status stating that there is not enough fuel for the journey*/
-        div.style.visibility = "visible"
-        if (fuelLevel < 10000)
+        
+        if (fuelLevel < 10000 && cargoLevel > 10000){
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+            launchStatus.innerHTML = "Shuttle not ready for launch";
+            launchStatus.style.color = "red";
+            cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+        }
+        
    
 }
-/* MY UNDERSTANDING--- We should prevent the form submission from happening until all inputs have valid values. We can use the event parameter and event.preventDefault() to stop the form submission
+}
+/* MY UNDERSTANDING--- We should prevent the form submission from happening until all inputs have valid values. We can use the event parameter and event.preventDefault() to stop the form submission. In order to read the value of an input, check the value attribute. Assign a new value to input.value which will update the value shown in the input.
 EXAMPLE:
 window.addEventListener("load", function() {
       let form = document.querySelector("form");
       form.addEventListener("submit", function(event) {
          let pilotNameInput = document.querySelector("input[name=pilotName]");
+         let pilot = pilotNameInput.value;
          let copilotNameInput = document.querySelector("input[name=copilotName]");
+         let copilot = copilotNameInput.value;
          let fuelInput = document.querySelector("input[name=fuelLevel]");
+         let fuelLevel = fuelInput.value;
          let cargoInput = document.querySelector("input[name=cargoMass]");
+         let cargoMass = cargoInput.value;
     (if statements would follow here)
           {
             alert("All fields are required!");
